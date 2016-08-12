777968d2d996bf060373cb32282da29118de5bb7
meisheep <immeisheep@gmail.com>
2016-08-11 20:00:38 -0700

    correct indention

--- inline-notes.php
+++ inline-notes.php
@@ -12,10 +12,10 @@ add_action( 'wp_enqueue_scripts', 'note_add_style' );
 add_shortcode( 'note', 'note_shortcode' );
 
 function note_add_style() {
-	wp_register_style( 'inline-notes-style', plugins_url('inline-notes-style.css', __FILE__) );
-  wp_enqueue_style( 'inline-notes-style' );
+    wp_register_style( 'inline-notes-style', plugins_url('inline-notes-style.css', __FILE__) );
+    wp_enqueue_style( 'inline-notes-style' );
 }
 
 function note_shortcode( $atts, $content = null ) {
-	return '<span class="inline-notes"><a class="inline-notes__button">⋯</a><span class="inline-notes__content">' . $content . '</span></span>';
+    return '<span class="inline-notes"><a class="inline-notes__button">⋯</a><span class="inline-notes__content">' . $content . '</span></span>';
 }
